e50a6ebfe2e0156cc352dc873476af5aeff6bf02
Paulo Machado Fraga Matos <paulo.machado.fraga.matos@gmail.com>
2023-02-02 22:00:36 -0300

    Enviando parte de validação

--- src/components/user/register/Register.tsx
+++ src/components/user/register/Register.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { SubmitHandler } from "react-hook-form/dist/types";
 
 const Register = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm<any>()
     const formValue:SubmitHandler<any> = (data:any) => console.log(data)
     return (
         <div className="mx-auto flex flex-col max-w-7xl pt-20 items-center">
@@ -13,23 +13,24 @@ const Register = () => {
                 <form onSubmit={handleSubmit(formValue)}>
                     <label className="block mb-6">
                         <span className="block mb-1 text-sm font-medium text-slate-700">Nome:</span>
-                        <input {...register('name')} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
+                        <input {...register('name', {maxLength: {message: 'Nome não deve possuir mais de 30 caracteres!' ,value:30}, required: true})} maxLength={40} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
+                        {errors.name?.type === 'maxLength' && <p>{errors.name.message?.toString()}</p>}
                     </label>
                     <label className="block mb-6">
                         <span className="block mb-1 text-sm font-medium text-slate-700">Nome de Usuario:</span>
-                        <input {...register('userName')} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
+                        <input {...register('userName',{maxLength: {message:'123',value:40}, required: true})} maxLength={40} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
                     </label>
                     <label className="block mb-6">
                         <span className="block mb-1 text-sm font-medium text-slate-700">E-mail:</span>
-                        <input {...register('email')} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
+                        <input {...register('email',{maxLength: {message:'123',value:40}, required: true})} maxLength={40} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
                     </label>
                     <label className="block mb-6">
                         <span className="block mb-1 text-sm font-medium text-slate-700">Senha:</span>
-                        <input {...register('senha')} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
+                        <input {...register('senha', {maxLength: {message:'123',value:40}, required: true})} maxLength={40} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
                     </label>
                     <label className="block mb-6">
                         <span className="block mb-1 text-sm font-medium text-slate-700">Confirm Senha:</span>
-                        <input {...register('confirmSenha')} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
+                        <input {...register('confirmSenha',{maxLength: {message:'123',value:40}, required:true})} maxLength={40} className="px-2 border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
                     </label>
                     <label className="block mb-6">
                         <input type="submit"  value="submit" className="text-slate-50 text-center px-2  bg-azul border border-slate-300 w-full py-2 border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
