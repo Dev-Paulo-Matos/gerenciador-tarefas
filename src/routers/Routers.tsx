178a23f4081a8d84b20b6c134500d1a3bcceaebb
Paulo Machado Fraga Matos <paulo.machado.fraga.matos@gmail.com>
2023-02-09 22:28:18 -0300

    Subindo a parte de controle de rota

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { randomBytes } from "crypto"
+import React, { Fragment, PropsWithChildren } from "react"
+import { BrowserRouter, Route, Routes, RedirectFunction, redirect, Navigate, useNavigate } from "react-router-dom"
 import { Header } from "../components/core/header/Header"
 import Main from "../components/core/main/Main"
 import { SectionContato } from "../components/sections/contato/Section-Contato"
@@ -33,6 +34,19 @@ const LoginPage = () => {
     )
 }
 
+const ProtectedRoute = (props: PropsWithChildren) => {
+    const navigate= useNavigate()
+    if(true){
+        navigate('/login')
+    }
+
+    return(
+    <React.Fragment>
+        { false ? props.children : <Navigate to="/gerenciador-tarefas/login" /> }
+    </React.Fragment>
+    )
+}
+
 export const Routers = () => {
     return (
         <BrowserRouter>
@@ -41,6 +55,11 @@ export const Routers = () => {
                 <Route path="/gerenciador-tarefas/*" element={<HomePage />} />
                 <Route path="/gerenciador-tarefas/register" element={<RegisterPage />} />
                 <Route path="/gerenciador-tarefas/login" element={<LoginPage />}/>
+                <Route path="/gerenciador-tarefas/app" element={
+                    <ProtectedRoute>
+                        123
+                    </ProtectedRoute>                
+                }/>
             </Routes>
         </BrowserRouter>
     )
